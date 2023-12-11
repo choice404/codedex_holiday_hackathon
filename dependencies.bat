baf5e98c7b519ccd52285878c15834d82b9dc224
choisauce <austinch20@protonmail.com>
2023-12-11 12:07:54 -0800

    Tools to help install dependencies for the project

---
+++ dependencies.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM This script is used to install the dependencies for the program
+
+REM Packages to install
+set packages=
+
+REM Check if python3 is already installed
+where python3 >nul 2>nul
+if %errorlevel% equ 0 (
+    echo Python3 is already installed
+) else (
+    echo Install Python3 from the Microsoft Store or from the python website at https://www.python.org
+)
+
+REM Install packages
+if defined packages[0] (
+    for %%p in (%packages%) do (
+        pip install %p
+    )
+) else (
+    echo No packages needed to install
+)
