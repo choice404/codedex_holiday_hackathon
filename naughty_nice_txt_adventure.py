25efc1f592545e68a065d4f8da02b9eebb2fdf6d
Yazmine Hoeferlin <hoeferlr@oregonstate.edu>
2023-12-12 22:56:35 -0500

    Naughty or Nice Morality Tracker Xmas themed game, Codedex Holiday Hackathon 12/12/23

--- naughty_nice_txt_adventure.py
+++ naughty_nice_txt_adventure.py
@@ -37,12 +37,12 @@ class NaughtyorNiceGame:
     def process_choice(self, choice, nice_outcome, naughty_outcome):
         """Sets values for morality trackers and stores Naughty or Nice values"""
 
-        if choice.lower() == 'nice' or 1:       # can choose nice or 1
+        if choice.lower() == 'nice':       # can choose nice or 1
             print(f"You choose the nice option: {nice_outcome}")
             self._morality_tracker += 1         # adding to trackers
             self._nice_tracker += 1
 
-        elif choice.lower() == 'naughty' or 2:
+        elif choice.lower() == 'naughty':
             print(f"You choose the naughty option: {naughty_outcome}")  # function to be able to modify it later
             self._morality_tracker -= 2         # can change values of tracker here
             self._naughty_tracker += 1
@@ -55,37 +55,37 @@ class NaughtyorNiceGame:
         """Can modify script and prompts or add more"""
 
         # Prompt 1: Caroling Chaos or Cheer?
-        self.display_prompt("A group of elves are caroling and spreading Yuletide cheer. You have a choice:",
+        self.display_prompt("\nA group of elves are caroling and spreading Yuletide cheer. You have a choice:",
                             ["You join in with the cheerful elves and sing along with them!",
                              "Bah Humbug! I'm not in the mood to sing..."])
 
-        choice_caroling = input("What's your decision? (Nice/Naughty): ")
+        choice_caroling = input("\nWhat's your decision? (Nice/Naughty): ")
 
-        self.process_choice(choice_caroling, "You join in the holiday fun!",
-                            "You are feeling mischievous today and end up throwing a snowball instead!")
+        self.process_choice(choice_caroling, "\nYou join in the holiday fun!",
+                            "\nYou are feeling mischievous today and end up throwing a snowball instead!")
 
         # Prompt 2: Snowball Fight
-        self.display_prompt("On the way back to Santa's workshop, you see some elves sledding. Do you:",
+        self.display_prompt("\nOn the way back to Santa's workshop, you see some elves sledding. Do you:",
                             ["Join in and have a blast!",
                              "Listen to the evil Krampus Polar Bear and throw snowballs of exploding ice at them!?"])
 
-        choice_snowball = input("What's your decision? (Nice/Naughty): ")
+        choice_snowball = input("\nWhat's your decision? (Nice/Naughty): ")
 
-        self.process_choice(choice_snowball, "You join in the fun!",
-                                "You embrace the chaos and throw an exploding ice snowball, causing a"
+        self.process_choice(choice_snowball, "\nYou join in the fun!",
+                                "\nYou embrace the chaos and throw an exploding ice snowball, causing a"
                                 " fight among the elves.")
 
         # Prompt 3: Feed Rudolph
-        self.display_prompt("As you continue back to Santa's workshop, Rudolph the Reindeer comes up to you.\n"
-                            "Rudolph needs his favorite chocolate chip cookies before the Christmas journey, "
+        self.display_prompt("\nAs you continue back to Santa's workshop, Rudolph the Reindeer comes up to you."
+                            "\nRudolph needs his favorite chocolate chip cookies before the Christmas journey, "
                             "so you head to the kitchen.",
                         ["Pick his favorite, chocolate chip.",
                             "Be sneaky and pick the oatmeal raisin!"])
 
-        choice_caroling = input("What's your decision? (Nice/Naughty): ")
+        choice_caroling = input("\nWhat's your decision? (Nice/Naughty): ")
 
-        self.process_choice(choice_caroling, "You picked Rudolph's favorite and Rudolph is beaming!",
-                        "Rudolph gets sick from his allergy to raisins and has to sit Christmas "
+        self.process_choice(choice_caroling, "\nYou picked Rudolph's favorite and Rudolph is beaming!",
+                        "\nRudolph gets sick from his allergy to raisins and has to sit Christmas "
                             "out this year!")
 
         # Display the morality tracker
